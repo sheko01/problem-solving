ec80849b692eac2fbd1cffc32c277812e4565a4d
shaker <169070494+sheko01@users.noreply.github.com>
2025-11-28 15:00:52 +0200

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -0,0 +1,9 @@
+// Last updated: 11/28/2025, 3:00:52 PM
+1class Solution {
+2public:
+3    bool isAnagram(string s, string t) {
+4        sort(s.begin(), s.end());
+5        sort(t.begin(), t.end());
+6        return s == t;
+7    };
+8};
