24051679e95eb1f51945f97f5a227e47bd569c18
shaker <169070494+sheko01@users.noreply.github.com>
2025-12-20 23:00:45 +0200

    File updated at 12/20/2025, 11:00:45 PM

--- 944-DeleteColumnstoMakeSorted/944-DeleteColumnstoMakeSorted.cpp
+++ 944-DeleteColumnstoMakeSorted/944-DeleteColumnstoMakeSorted.cpp
@@ -1,16 +1,16 @@
-// Last updated: 12/20/2025, 9:32:23 PM
+// Last updated: 12/20/2025, 11:00:45 PM
 1class Solution {
 2public:
 3    int minDeletionSize(vector<string>& strs) {
 4        int faults = 0;
 5        int col = strs[0].length();
 6        for (int i = 0; i < col; i++) {
-7            for (int j = 0; j < strs.size(); j++) {
-8                if ((j + 1) < strs.size()) {
+7            for (int j = 0; j < strs.size()-1; j++) {
+8                
 9                    if (strs[j][i] > strs[j + 1][i]) {
 10                        faults++;
 11                        break;
-12                    }
+12                    
 13                }
 14            }
 15        }
