0a64956fb338abb3a1899c003dc60c72d1521723
shaker <169070494+sheko01@users.noreply.github.com>
2025-12-25 15:01:59 +0200

    Create 3075-MaximizeHappinessofSelectedChildren

---
+++ 3075-MaximizeHappinessofSelectedChildren/3075-MaximizeHappinessofSelectedChildren.cpp
@@ -0,0 +1,20 @@
+// Last updated: 12/25/2025, 3:01:59 PM
+1class Solution {
+2public:
+3    long long maximumHappinessSum(vector<int>& happiness, int k) {
+4        sort(happiness.begin(), happiness.end());
+5        int dec = 0;
+6        unsigned long sum = 0;
+7        while (k > 0) {
+8            if ((happiness.back() - dec) > 0) {
+9                sum += (happiness.back() - dec);
+10                happiness.pop_back();
+11                dec++;
+12                k--;
+13            } else {
+14                break;
+15            }
+16        }
+17        return sum;
+18    }
+19};
