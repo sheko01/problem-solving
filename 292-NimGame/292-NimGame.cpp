3b025ea3d04b261254e2f65459229fdfd3aae9eb
shaker <169070494+sheko01@users.noreply.github.com>
2025-12-26 14:59:15 +0200

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.cpp
@@ -0,0 +1,10 @@
+// Last updated: 12/26/2025, 2:59:15 PM
+1class Solution {
+2public:
+3    bool canWinNim(int n) {
+4        if(n%4==0){
+5            return false;
+6        }
+7        return true;
+8    }
+9};
